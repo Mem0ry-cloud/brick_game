5147fe1ea37759a5bc264d72a2b96ee92ea715ec
Mem0ry-cloud <76276914+Mem0ry-cloud@users.noreply.github.com>
2023-01-17 15:19:12 +0300

    interface v0.1

---
+++ classes.py
@@ -0,0 +1,48 @@
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Size:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+
+class Cursor:
+    pass
+
+
+class Label:
+    def __init__(self, position, size, color, text):
+        self.position = position
+        self.size = size
+        self.color = color
+        self.text = text
+        pass
+
+
+class Button:
+    def __init__(self, position, size, color, on_click, label=None):
+        self.position = position
+        self.size = size
+        self.color = color
+        self.label = label
+        pass
+
+
+class GameObject:
+    pass
+
+
+class Brick(GameObject):
+    pass
+
+
+class Ball(GameObject):
+    pass
+
+
+class Platform(GameObject):
+    pass
---
+++ windows.py
@@ -0,0 +1,22 @@
+class Window:
+    pass
+
+
+class MainMenuWindow(Window):
+    pass
+
+
+class CampaignMenuWindow(Window):
+    pass
+
+
+class LevelsMenuWindow(Window):
+    pass
+
+
+class MainGameWindow(Window):
+    pass
+
+
+class CreateMapWindow(Window):
+    pass
