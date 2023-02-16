53c6984aed378de3ff3cd0ebe3fca9575db57e93
DragonZero000 <psp4games@mail.ru>
2023-02-16 19:15:55 +0300

    цвета блоков изменены

--- classes.py
+++ classes.py
@@ -24,7 +24,9 @@ class Cursor:
 
     def get_map(self, map_name, form='list'):
         sym = ["0", "@", "*", "#", '&', '^', '%', '!', '/', '<', '>', '?']
-        format_sym = [[0, 0], [-1, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0]]
+        format_sym = [[0, (255, 255, 255)], [-1, (0, 0, 0)], [1, (255, 0, 0)], [2, (255, 165, 0)], [3, (255, 255, 0)],
+                      [4, (255, 255, 153)], [5, (0, 128, 0)], [6, (8, 189, 255)], [7, (8, 82, 255)], [8, (8, 99, 255)],
+                      [9, (0, 0, 255)], [10, (102, 11, 186)]]
         map_key = self.get_info("key", map_name)
         map_file = open(map_key, encoding='utf8')
         map_coded = map_file.read()
@@ -32,10 +34,7 @@ class Cursor:
         for i in map_coded:
             symbol_index = sym.index(i)
             map_uncoded.append(format_sym[symbol_index])
-        if form is not 'list':
-            pass
-        else:
-            return map_uncoded
+        return map_uncoded
 
 
 class Label(pg.sprite.Sprite):
